b9c099cb4e1b8c3cc13e9b64bf33c325d5f8c8b1
MonikaDobreva <m.dobreva@student.fontys.nl>
2021-10-31 17:53:31 +0100

    improve linked list with constructor and start iterator

--- assignment/implementation/src/main/java/appointmentplannerimpl/DoublyLinkedList.java
+++ assignment/implementation/src/main/java/appointmentplannerimpl/DoublyLinkedList.java
@@ -1,9 +1,27 @@
 package appointmentplannerimpl;
 
-public class DoublyLinkedList<T> {
+import java.util.Iterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class DoublyLinkedList<T> implements Iterable<T>{
     AllocationNode<T> head = null;
     AllocationNode<T> tail = null;
     private int size;
+
+    public DoublyLinkedList(){
+        //initialize head and tail
+        head = new AllocationNode<T>(null);
+        tail = new AllocationNode<T>(null);
+        //set head and tail to be the same
+        head.next = tail;
+        tail.previous = head;
+        //set head and tail to be the only thing in the list
+        head.previous = null;
+        tail.next = null;
+        //size of the list is 0 at this moment
+        size = 0;
+    }
 
     public void addNode(T item) {
         AllocationNode<T> newNode = new AllocationNode<>(item);
@@ -22,5 +40,10 @@ public class DoublyLinkedList<T> {
 
     public int getSize() {
         return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
