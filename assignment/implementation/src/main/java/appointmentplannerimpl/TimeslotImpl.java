543a5158be20c46fbec93ea16fc491fb0718ea57
MonikaDobreva <m.dobreva@student.fontys.nl>
2021-10-31 18:08:28 +0100

    improve timeslslot with exceptions and equals&hashcode implementation

--- assignment/implementation/src/main/java/appointmentplannerimpl/TimeslotImpl.java
+++ assignment/implementation/src/main/java/appointmentplannerimpl/TimeslotImpl.java
@@ -4,12 +4,22 @@ import appointmentplanner.api.TimeSlot;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Objects;
 
 public class TimeslotImpl implements TimeSlot {
     private Instant start;
     private Instant end;
 
-    public TimeslotImpl(Instant start, Instant end) {
+    public TimeslotImpl(Instant start, Instant end) throws IllegalArgumentException {
+        if (start == null){
+            throw new IllegalArgumentException("The start cannot be null!");
+        }
+        if (end == null){
+            throw new IllegalArgumentException("The end cannot be null!");
+        }
+        if (end.isBefore(start)){
+            throw new IllegalArgumentException("The star must be before the end!");
+        }
         this.start = start;
         this.end = end;
     }
@@ -31,5 +41,18 @@ public class TimeslotImpl implements TimeSlot {
     @Override
     public String toString() {
         return "Start: " + this.start + ", end: " + this.end + ", duration: " + duration() + ".";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TimeslotImpl timeslot = (TimeslotImpl) o;
+        return start.equals(timeslot.start) && end.equals(timeslot.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
     }
 }
