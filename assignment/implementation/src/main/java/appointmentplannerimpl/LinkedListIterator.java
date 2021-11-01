a763e450042ca1889b6889260cba89efcc309c2f
MonikaDobreva <m.dobreva@student.fontys.nl>
2021-11-01 01:06:05 +0100

    create constructor for the iterator

--- assignment/implementation/src/main/java/appointmentplannerimpl/LinkedListIterator.java
+++ assignment/implementation/src/main/java/appointmentplannerimpl/LinkedListIterator.java
@@ -1,4 +1,23 @@
 package appointmentplannerimpl;
 
-public class LinkedListIterator {
+import java.util.Iterator;
+
+public class LinkedListIterator<T> implements Iterator {
+    private AllocationNode<T> node;
+    private AllocationNode<T> tail;
+
+    public LinkedListIterator(AllocationNode<T> head, AllocationNode<T> tail){
+        this.node = head;
+        this.tail = tail;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
 }
