31ec4fbf582e048edaea821f001abc59d041d6ab
MonikaDobreva <m.dobreva@student.fontys.nl>
2021-11-02 11:02:00 +0100

    add more TimeslotImpl tests

--- assignment/implementation/src/test/java/appointmentplanner/TimeslotImplTest.java
+++ assignment/implementation/src/test/java/appointmentplanner/TimeslotImplTest.java
@@ -1,16 +1,32 @@
 package appointmentplanner;
 
+import appointmentplannerimpl.TimeslotImpl;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
 import java.time.Instant;
 
 public class TimeslotImplTest {
-    private Instant start;
-    private Instant end;
+    private Instant start = Instant.now().plusSeconds(36000);
+    private Instant end = start.plusSeconds(3600);
+    private TimeslotImpl timeSlot = new TimeslotImpl(start, end);
 
     @Test
     public void DurationTest(){
-        //assertThat.()
+        assertThat(this.timeSlot.duration())
+                .isEqualTo(Duration.ofMinutes(60));
+    }
+
+    @Test
+    public void getStartTest() {
+        assertThat(this.timeSlot.getStart())
+                .isEqualTo(this.start);
+    }
+
+    @Test
+    public void getEndTest() {
+        assertThat(this.timeSlot.getEnd())
+                .isEqualTo(this.end);
     }
 }
