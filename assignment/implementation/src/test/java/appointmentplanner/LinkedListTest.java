e50130239b271149a4199f755ec0c2df0397f30c
MonikaDobreva <m.dobreva@student.fontys.nl>
2021-11-04 13:30:26 +0100

    setUp LinkedListTest

--- assignment/implementation/src/test/java/appointmentplanner/LinkedListTest.java
+++ assignment/implementation/src/test/java/appointmentplanner/LinkedListTest.java
@@ -1,5 +1,14 @@
 package appointmentplanner;
 
-public class LinkedListTest {
+import appointmentplanner.api.TimeSlot;
+import appointmentplannerimpl.DoublyLinkedList;
+import org.junit.jupiter.api.BeforeEach;
 
+public class LinkedListTest {
+    private DoublyLinkedList<TimeSlot> list;
+
+    @BeforeEach
+    private void setUp() {
+        list = new DoublyLinkedList<TimeSlot>();
+    }
 }
