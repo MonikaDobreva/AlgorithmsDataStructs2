d9f5316b17c322ec612bfd829e7be09a411c987f
MonikaDobreva <m.dobreva@student.fontys.nl>
2021-11-04 14:23:05 +0100

    tests for linked list size

--- assignment/implementation/src/test/java/appointmentplanner/LinkedListTest.java
+++ assignment/implementation/src/test/java/appointmentplanner/LinkedListTest.java
@@ -3,6 +3,10 @@ package appointmentplanner;
 import appointmentplanner.api.TimeSlot;
 import appointmentplannerimpl.DoublyLinkedList;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LinkedListTest {
     private DoublyLinkedList<TimeSlot> list;
@@ -10,5 +14,18 @@ public class LinkedListTest {
     @BeforeEach
     private void setUp() {
         list = new DoublyLinkedList<TimeSlot>();
+    }
+
+    @Test
+    public void isEmptyTrue() {
+        assertThat(this.list.getSize())
+            .isEqualTo(0);
+    }
+
+    @Test
+    public void isEmptyFalse() {
+        this.list.toFront(mock(TimeSlot.class));
+        assertThat(this.list.getSize())
+                .isNotEqualTo(0);
     }
 }
